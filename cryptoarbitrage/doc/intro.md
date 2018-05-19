f75862f44be7f006f2138846b6ca84af02d61414
Stevan Koncar <beast.of.burden@hotmail.com>
2018-05-19 23:00:51 +0200

    Project init

---
+++ cryptoarbitrage/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cryptoarbitrage
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ cryptoarbitrage/src/cryptoarbitrage/core.clj
@@ -0,0 +1,11 @@
+(ns cryptoarbitrage.core
+  (:gen-class))
+
+
+(defn -main
+  "method information"
+  [& args]
+
+  (println "Hello World")
+
+  )
